d158f2e6626d7699b2f657bffb57c34cf4ccf07b
JackHamilton01 <jackhamilton2807@gmail.com>
2023-01-23 13:13:08 +0000

    All files within a single module and folders created

---
+++ ViewModelLocator/Locator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelLocator
+{
+    public class Locator : ILocator
+    {
+        public string LocateFile(string path, string solutionPath)
+        {
+            return "";
+        }
+
+        public string LocateDataContext()
+        {
+            return "";
+        }
+    }
+}
