d0cc3480a9a18f8b4f1dfdd4927b077cf8b7481d
JackHamilton01 <jackhamilton2807@gmail.com>
2023-01-23 13:16:53 +0000

    Fixed previous pull request

---
+++ MvvmHelpKit/Services/FileFinderService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace FileNavigation
+{
+    public class FileFinderService
+    {
+        public string FindCorrectPotentialFile(string activeDocument, string solutionPath)
+        {
+           var files = Directory
+                .EnumerateFiles(solutionPath)
+                .Where(file => file.ToLower().EndsWith(FileTypes.Cs) || file.ToLower().EndsWith(FileTypes.Xaml))
+                .ToList();
+
+            if (activeDocument.Contains(MvvmTypes.ViewModel))
+            {
+                return filesContainingActiveDocumentName(files, activeDocument, MvvmTypes.View);
+            }
+            else
+            {
+                return filesContainingActiveDocumentName(files, activeDocument, MvvmTypes.ViewModel);
+            }
+        }
+
+        private string RemoveEverythingAfterView(string activeDocument)
+        {
+            var word = activeDocument.Substring(0, activeDocument.LastIndexOf(MvvmTypes.View));
+            return word;
+        }
+
+        private string filesContainingActiveDocumentName(List<string> files, string activeDocument, string type)
+        {
+            var activeDocumentName = RemoveEverythingAfterView(activeDocument);
+            return files.Where(a => a.Contains(activeDocumentName) && a.Contains(type)).FirstOrDefault();
+        }
+    }
+}
---
+++ MvvmHelpKit/Types/MvvmTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNavigation
+{
+    public static class MvvmTypes
+    {
+        public static readonly string View = nameof(View);
+        public static readonly string ViewModel = nameof(ViewModel);
+    }
+}
